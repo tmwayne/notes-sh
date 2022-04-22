afe4e6cb69f2a4fdb13e4aa74fcc703407c762a0
Tyler Wayne <tylerwayne3@gmail.com>
2022-04-21 22:35:52 -0700

    clean code for print help text. make non-constant variables lowercase

--- notes.sh
+++ notes.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 #
 # ------------------------------------------------------------------------------
-# Notes
+# notes.sh
 # ------------------------------------------------------------------------------
 #
-# Tyler Wayne © 2020
+# Tyler Wayne © 2022
 #
 
-CUR_DIR=$( dirname $0 )
 THIS_PROG=$( basename $0 )
 USAGE="Usage: $THIS_PROG note [...]"
 
-Help() {
-  # Function to display help at command line
-  echo $USAGE
-  echo "Edit notes"
-  echo
-  echo "Options:"
-  echo "  -h, --help                Print this help."
-  echo
-}
+HELP="\
+$USAGE
+Edit notes
+
+Options:
+  -h, --help                Print this help.
+"
 
 # Arguments --------------------------------------------------------------------
 
@@ -38,7 +35,7 @@ done
 OPTIND=1
 while getopts ":h" opt; do
   case $opt in
-    h)  Help; exit 0 ;;
+    h)  echo "$HELP"; exit 0 ;;
     \?) echo "$THIS_PROG: unrecognized option '-$OPTARG'" >&2
         echo "Try '$THIS_PROG --help' for more information."
         exit 2 ;;
@@ -47,11 +44,11 @@ done
 shift $((OPTIND-1))
 
 
-NOTES_DIR=${NOTES_DIR:-$HOME/docs/notes/}
+notes_dir=${NOTES_DIR:-$HOME/docs/notes/}
 
-ARGS=($@) # Accept a list of notes
-NOTES=(${ARGS[@]/#/$NOTES_DIR})
-NOTES=${NOTES[@]/%/.txt}
+args=($@) # Accept a list of notes
+notes=(${args[@]/#/$notes_dir})
+notes=${notes[@]/%/.txt}
 
 # Assertions -------------------------------------------------------------------
 
@@ -62,4 +59,4 @@ fi
 
 # Main -------------------------------------------------------------------------
 
-vim $NOTES # Create a new note if one isn't found
+vim $notes # Create a new note if one isn't found
